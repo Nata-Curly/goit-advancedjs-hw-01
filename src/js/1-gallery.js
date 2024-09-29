80a4f2cf2a29de506b27b715d70d0534f3fbd4d3
Natalia Kucheriava <nkucheryava@gmail.com>
2024-09-29 20:54:07 +0200

    init

---
+++ src/js/1-gallery.js
@@ -0,0 +1,28 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import images from '../data/images';
+
+const gallery = document.querySelector('.gallery');
+
+
+function createGAlleryCard({ preview, original, description }) {
+  return `<li class="gallery-item">
+  <a class="gallery-link" href="${original}">
+    <img class="gallery-image" src="${preview}" alt="${description}" />
+  </a>
+</li>`;
+}
+
+const galleryMarkup = images.map(createGAlleryCard).join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  enableKeyboard: true,
+  animationSlide: true,
+  overlay: true,
+});
---
+++ src/js/2-form.js
@@ -0,0 +1,52 @@
+const STORAGE_KEY = 'feedback-form-state';
+const feedbackForm = document.querySelector('.feedback-form');
+
+const formData = {
+  email: '',
+  message: '',
+};
+
+fillFromLS(feedbackForm);
+
+feedbackForm.addEventListener('input', handleFormInput);
+feedbackForm.addEventListener('submit', handleFormSubmit);
+
+function fillFromLS(form) {
+  const dataFromLS = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (dataFromLS === null) {
+    return;
+  }
+
+  for (const key in dataFromLS) {
+    form.elements[key].value = dataFromLS[key];
+    formData[key] = dataFromLS[key];
+  }
+}
+
+function handleFormInput(event) {
+  const inputName = event.target.name;
+  const inputValue = event.target.value.trim();
+
+  formData[inputName] = inputValue;
+
+  try {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log(formData);
+  localStorage.removeItem(STORAGE_KEY);
+    event.target.reset();
+    formData.email = '';
+    formData.message = '';
+}
